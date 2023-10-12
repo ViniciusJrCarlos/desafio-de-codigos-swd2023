3fe02730c3be8a092def1440746c764bbd099a05
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-10-12 02:19:32 -0300

    enviando desafio de herança

---
+++ src/HerancaBancaria/Heranca.java
@@ -0,0 +1,75 @@
+package HerancaBancaria;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Heranca {
+
+
+    public static void main(String[] args) {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in);
+        String titular = scanner.next();
+        int numeroConta = scanner.nextInt();
+        double saldo = scanner.nextDouble();
+        double taxaJuros = scanner.nextDouble();
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+
+        System.out.println("Conta Poupanca:");
+        contaPoupanca.exibirInformacoes();
+    }
+}
+
+class ContaBancaria {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+
+    public ContaBancaria(int numero, String titular, double saldo, double taxaJuros) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void exibirInformacoes() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println(titular);
+        System.out.println(numero);
+        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    private double taxaJuros;
+   //this.taxaJuros = taxaJuros;
+
+    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+
+        //TODO: Implementar adequadamente esta sobrecarga de construtores.
+        //this.numero = numero;
+        //this.titular = titular;
+        //this.saldo = saldo;
+        //this.taxaJuros = taxaJuros;
+
+        super(numero, titular, saldo, taxaJuros);
+        //ContaBancaria();
+        //ContaBancaria.numero();
+        this.taxaJuros = taxaJuros;
+
+    }
+
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Taxa de juros: " + taxaJuros + "%");
+
+        //TODO: Complementar as informações com a taxa de juros.
+        //System.out.println("Conta: " + super.numero);
+       // System.out.println("Titular: " + super.titular);
+        //System.out.printf("Saldo: R$ %.1f%n", super.saldo);
+       // System.out.printf("Taxa de Juros: ", ,"%");
+
+    }
+}
+
